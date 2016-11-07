daa0fe7512c75043ba2f6ad5ee2de6fc240f23b7
Marco Roganovic <user@News-MacBook.fritz.box>
2016-11-07 19:47:01 +0100

    Finally working version

--- js/app.js
+++ js/app.js
@@ -12,9 +12,68 @@
       $script = $iframe.contentWindow.document.createElement("script");
 
   $script.type = "text/javascript";
-  
+  $iframeHead.appendChild($script);
+
   var $toggleButton = document.getElementsByClassName("header-toggle")[0],
       $clearFieldsButton = document.getElementsByClassName("header-clear")[0];
+
+ 
+  var htmlEditor = CodeMirror.fromTextArea($htmlEditor, {
+    lineNumbers: true,
+    tabSize: 2,
+    theme: 'neo',
+    lineWrapping: true,
+    mode: "text/html"
+  });
+  
+  var cssEditor = CodeMirror.fromTextArea($cssEditor, {
+    lineNumbers: true,
+    tabSize: 2,
+    lineWrapping: true,
+    theme: 'neo',
+    mode: "text/css"
+  });
+
+  var jsEditor = CodeMirror.fromTextArea($jsEditor, {
+    lineNumbers: true,
+    tabSize: 2,
+    lineWrapping: true,
+    mode: "text/javascript",
+    theme: 'neo'
+  });
+
+  htmlEditor.on("keyup", function(cm, evt) {
+    $iframeBody.innerHTML = htmlEditor.getValue();
+  });
+
+  cssEditor.on("keyup", function(cm, evt) {
+      $style.innerHTML = cssEditor.getValue();;
+      $iframeHead.appendChild($style);
+  });
+  
+
+  jsEditor.on("keyup", function(cm, evt) {
+     var script = $iframeHead.getElementsByTagName("script")[0];
+     script.parentNode.removeChild(script);
+     
+     var newScript = $iframe.contentDocument.createElement("script");
+     newScript.textContent = jsEditor.getValue();
+     $iframeHead.appendChild(newScript);
+     console.clear();
+  });
+
+  $clearFieldsButton.addEventListener("click", function() {
+    if(confirm("Are you sure")) {
+      htmlEditor.setValue("");
+      cssEditor.setValue("");
+      jsEditor.setValue("");
+      $iframeHead.innerHTML = "";
+      $iframeBody.innerHTML = "";
+    }
+    console.log('cleared');
+  });
+
+  
 
   $toggleButton.addEventListener("click", function() {
     if($editor.style.display === "none") {
@@ -23,52 +82,15 @@
       $editor.style.display = "none";
     }
   });
-  
-  $clearFieldsButton.addEventListener("click", function() {
-    if(confirm("Are you sure")) {
-      $iframeHead.innerHTML = "";
-      $iframeBody.innerHTML = "";
+ 
+  var setHeightOnElements = function(opts) {
+    var elms = document.getElementsByClassName(opts.className);
+   
+    for(var i = 0, n = elms.length; i < n; i++) {
+      elms[i].style.height = opts.height.toString() + "px";
     }
-    console.log('cleared');
-  });
+  }
 
-  var htmlEditor = CodeMirror.fromTextArea($htmlEditor, {
-    lineNumbers: true,
-    tabSize: 2,
-    theme: 'elegant',
-    lineWrapping: true
-  });
-
-  var cssEditor = CodeMirror.fromTextArea($cssEditor, {
-    lineNumbers: true,
-    tabSize: 2,
-    lineWrapping: true,
-    theme: 'elegant'
-  });
-
-  var jsEditor = CodeMirror.fromTextArea($jsEditor, {
-    lineNumbers: true,
-    tabSize: 2,
-    lineWrapping: true,
-    mode: "javascript",
-    themes: 'elegant'
-  });
-
-  htmlEditor.on("change", function(cm, evt) {
-    console.log(cm, evt); 
-    $iframeBody.innerHTML = htmlEditor.getValue();
-  });
-
-  cssEditor.on("change", function(cm, evt) {
-      $style.innerHTML = cssEditor.getValue();;
-      $iframeHead.appendChild($style);
-  });
-  
-
-  jsEditor.on("change", function(cm, evt) {
-     $script.textContent  = jsEditor.getValue();
-     $iframeHead.appendChild($script);
-  });
-  
+  setHeightOnElements({className: "CodeMirror", height: 173});
 
 })();
