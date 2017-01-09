4617302441d3f9664038f05d08a1cd83f39ce480
Marco Roganovic <marcoroganovic@gmail.com>
2017-01-09 16:59:22 +0100

    add cleanCSS task

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require("gulp");
 var concat = require("gulp-concat");
 var cssConcat = require("gulp-concat-css");
+var cleanCSS = require("gulp-clean-css");
 var uglify = require("gulp-uglify");
 var pump = require("pump");
 
@@ -28,8 +29,10 @@ gulp.task("scripts", function() {
 gulp.task("css", function() {
   return gulp.src(cssPaths)
         .pipe(cssConcat("css/bundle.css"))
+        .pipe(cleanCSS())
         .pipe(gulp.dest("dist"));
 });
+
 
 gulp.task("compress", function(cb) {
   pump([
